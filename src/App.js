9f2be4b98f899ca6809ee5285a23ad0112d47938
anthony0br <129159900+anthony0br@users.noreply.github.com>
2023-04-02 03:40:53 +0100

    Added results block and input validation

--- src/App.js
+++ src/App.js
@@ -2,20 +2,62 @@ import './App.css';
 import React, { useState }  from 'react';
 
 function CalculateYearsForm(props) {
-  const [value, setValue] = useState('');
+  // Input hooks
+  const [loanAmount, setLoanAmount] = useState("42000");
+  const [startingSalary, setSalary] = useState("25000");
+
+  // Output hooks
+  const [loanClass, setLoanClass] = useState("");
+  const [resultsDisplay, setResultsDisplay] = useState("none");
+  const [yearsResult, setYearsResult] = useState("");
 
   // Calculate number of years, set states of results component to display results
   function handleSubmit(submitEvent) {
-    alert('Form input submited! It says: ' + value);
     submitEvent.preventDefault();
+
+    // Check for empty or invalid amounts
+    let loanRemaining = Number(loanAmount)
+    if (loanAmount === "" || Number.isNaN(loanRemaining) || loanRemaining < 0) {
+      setLoanClass("invalid");
+      return;
+    }
+
+    // Set textboxes to valid
+    setLoanClass("");
+
+
+    // Display results
+    setResultsDisplay("block");
   }
+
+  const resultsStyle = {
+    display: resultsDisplay,
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="number" value={value} onChange={event => {setValue(event.target.value)}} placeholder={props.placeholder} />
+      <h1>Number of Years Calculator</h1>
+      <p>Currently, this app only calculates the number of years it will take to repay a loan. Calculators for other missing variables will be added later.</p>
+      
+      <h3>Total loan to pay</h3>
+      <p>The total outstanding amount you need to pay, including maintenance and tuition.</p>
+      <input type="number" className={loanClass} value={loanAmount} onChange={event => {setLoanAmount(event.target.value)}} />
+
+      <h3>Salary modelling</h3>
+      <p>This is a simple model of your projected salary. Each year the salary increases by salaryIncrease + salary * salaryGrowth.</p>
+      <p>Salary growth: </p>
+      <input type="number" value={startingSalary} onChange={event => {setSalary(event.target.value)}} />
+      <p>Salary increase: </p>
+      <input type="number" value={startingSalary} onChange={event => {setSalary(event.target.value)}} />
       <br></br>
       <br></br>
       <button type="submit">Calculate</button>
+      <br></br>
+      <br></br>
+      <div className="results" style={resultsStyle}>
+        <h1>Results</h1>
+        <p>It will take roughly {yearsResult} to pay off your plan 5 student loan. Note that after 40 years, your debt will be wiped.</p>
+      </div>
     </form>
   );
 }
@@ -29,7 +71,7 @@ function App() {
         </p>
       </header>
       <body className="App-body">
-        <CalculateYearsForm placeholder="placeholder text!"></CalculateYearsForm>
+        <CalculateYearsForm></CalculateYearsForm>
       </body>
       <footer className="App-footer">
         <p>
@@ -38,10 +80,9 @@ function App() {
           Designed to estimate repayment information for plan 5 student loans.
           Assumptions such as rate of RPI, CPI and future government policy have been made.
           <br></br>
-          Last update April 2023.
+          Licensed under GNU GPL-3.0. Last update April 2023.
         </p>
         <a href="https://github.com/anthony0br/plan-5-loan-calculator">View Github repository</a>
-        <br/>
       </footer>
     </div>
   );
