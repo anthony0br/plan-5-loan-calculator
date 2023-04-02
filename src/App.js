0a56980dc6d1882169822272f29bd58d54fe345c
anthony0br <129159900+anthony0br@users.noreply.github.com>
2023-04-02 04:19:53 +0100

    Completed form and validation

--- src/App.js
+++ src/App.js
@@ -4,29 +4,54 @@ import React, { useState }  from 'react';
 function CalculateYearsForm(props) {
   // Input hooks
   const [loanAmount, setLoanAmount] = useState("42000");
-  const [startingSalary, setSalary] = useState("25000");
+  const [startingSalary, setStartingSalary] = useState("25000");
+  const [salaryGrowth, setSalaryGrowth] = useState("4");
+  const [salaryIncrease, setSalaryIncrease] = useState("2000");
 
   // Output hooks
   const [loanClass, setLoanClass] = useState("");
+  const [salaryClass, setSalaryClass] = useState("");
+  const [increaseClass, setIncreaseClass] = useState("");
+  const [growthClass, setGrowthClass] = useState("");
   const [resultsDisplay, setResultsDisplay] = useState("none");
   const [yearsResult, setYearsResult] = useState("");
 
   // Calculate number of years, set states of results component to display results
   function handleSubmit(submitEvent) {
+    // Hide old results and prevent refreshing
     submitEvent.preventDefault();
+    setResultsDisplay("none");
 
-    // Check for empty or invalid amounts
+    // Check for empty or invalid amounts and cast inputs
     let loanRemaining = Number(loanAmount)
     if (loanAmount === "" || Number.isNaN(loanRemaining) || loanRemaining < 0) {
       setLoanClass("invalid");
+      return;
+    }
+    let salary = Number(startingSalary)
+    if (startingSalary === "" || Number.isNaN(salary) || salary < 0) {
+      setSalaryClass("invalid");
+      return;
+    }
+    let increase = Number(salaryIncrease)
+    if (loanAmount === "" || Number.isNaN(increase) || increase < 0) {
+      setIncreaseClass("invalid");
+      return;
+    }
+    let growth = Number(salaryGrowth)
+    if (loanAmount === "" || Number.isNaN(growth) || growth < 0) {
+      setGrowthClass("invalid");
       return;
     }
 
     // Set textboxes to valid
     setLoanClass("");
 
+    // Calculate result
+    let numYears = 0;
 
     // Display results
+    setYearsResult(numYears);
     setResultsDisplay("block");
   }
 
@@ -37,18 +62,20 @@ function CalculateYearsForm(props) {
   return (
     <form onSubmit={handleSubmit}>
       <h1>Number of Years Calculator</h1>
-      <p>Currently, this app only calculates the number of years it will take to repay a loan. Calculators for other missing variables will be added later.</p>
+      <p>Currently, this app only calculates the number of years it will take to repay a loan, ignoring the write-off after 40 years. It will also output your final salary, to help you adjust the salary model. Calculators for other missing variables will be added later.</p>
       
       <h3>Total loan to pay</h3>
       <p>The total outstanding amount you need to pay, including maintenance and tuition.</p>
       <input type="number" className={loanClass} value={loanAmount} onChange={event => {setLoanAmount(event.target.value)}} />
 
       <h3>Salary modelling</h3>
-      <p>This is a simple model of your projected salary. Each year the salary increases by salaryIncrease + salary * salaryGrowth.</p>
-      <p>Salary growth: </p>
-      <input type="number" value={startingSalary} onChange={event => {setSalary(event.target.value)}} />
-      <p>Salary increase: </p>
-      <input type="number" value={startingSalary} onChange={event => {setSalary(event.target.value)}} />
+      <p>This is a simple model of your projected salary. It assumes your salary will increase by a fixed amount and then increase by a fixed percent each year.</p>
+      <p>Starting salary (£):</p>
+      <input type="number" className={salaryClass} value={startingSalary} onChange={event => {setStartingSalary(event.target.value)}} />
+      <p>Salary growth (%) per year: </p>
+      <input type="number" className={growthClass} value={salaryGrowth} onChange={event => {setSalaryGrowth(event.target.value)}} />
+      <p>Salary increase (£) per year: </p>
+      <input type="number" className={increaseClass} value={salaryIncrease} onChange={event => {setSalaryIncrease(event.target.value)}} />
       <br></br>
       <br></br>
       <button type="submit">Calculate</button>
